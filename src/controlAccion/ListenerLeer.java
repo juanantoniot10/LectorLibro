076702ff930b2d72b4b8b44b3335b8fee601ae79
Juanantoniot10 <juanantoniot10hithub@gmail.com>
2018-05-14 18:25:02 +0200

    desconcetar lector  implementado

--- src/controlAccion/ListenerLeer.java
+++ src/controlAccion/ListenerLeer.java
@@ -15,7 +15,6 @@ public class ListenerLeer implements ActionListener{
 
 	private boolean sonando = false;
 	private SwingWorker worker;
-	private Player reproductor;
 	private PanelLectura panelLectura;
 	private SimpleTTS voz;
 	public ListenerLeer(PanelLectura panelLectura) {
@@ -32,13 +31,21 @@ public class ListenerLeer implements ActionListener{
 					@Override
 					protected Object doInBackground() throws Exception {
 						voz = new SimpleTTS("kevin");
-						voz.speak("aun es toi por me ho rar. por que jo ha blo mu e ra ro. pe ro te vo e ale er el pWho tho li bro."+panelLectura.getTextPaneLeft().getText()+panelLectura.getTextPaneDer().getText());
-						voz.close();
+						voz.speak("aun es toi por me ho rar. por que jo ha blo mu e ra ro. pe ro te vo e ale er el pho tou li bro."+panelLectura.getTextPaneLeft().getText()+panelLectura.getTextPaneDer().getText());
 						return null;
 					}
 				};	
 				worker.execute();
 				sonando = true;
+			} else {
+				try {
+					voz.close();
+				} catch (Exception e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				sonando=false;
 			}
+				
 		}
 }
